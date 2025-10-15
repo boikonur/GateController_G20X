ed33e192ef5ea236d25b0431bf72bb70db91817e
Boyko Kazakov <boikonur@gmail.com>
2025-10-15 14:09:42 +0300

    Make Index

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import G202SmsTool from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <G202SmsTool />
+  </React.StrictMode>
+);
